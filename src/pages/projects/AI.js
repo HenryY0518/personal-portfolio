564f22f0e906d9cabfcb4ea14534710828ffca5e
HenryY0518 <65589440+HenryY0518@users.noreply.github.com>
2022-03-04 21:14:04 -0800

    updated AI Garden

--- src/pages/projects/AI.js
+++ src/pages/projects/AI.js
@@ -14,6 +14,7 @@ import ai_wireframe from '../../assets/images/ai/ai_wireframe.jpeg'
 import style1 from '../../assets/images/ai/style1.jpeg'
 import prototype1 from '../../assets/images/ai/prototype1.jpeg'
 import team1 from '../../assets/images/ai/team1.jpeg'
+import malfunction from '../../assets/images/ai/malfunction.jpeg'
 
 
 
@@ -32,11 +33,10 @@ function AI() {
 
             <Text head={'Background'}>
                 <p>
-                    I went back to China to be with my family during the pandemic and was able to land an UX internship at a local start-up. The start-up, with garden design background, was pivoting into a tech company by the time I joined.
+                    I went back to China to be with my family during the pandemic and was able to land a UX internship at a local start-up. The start-up, with garden design background, was pivoting into a tech company by the time I joined.
                 </p>
                 <p>
-                    According to the research of AI Garden, there is a growing population of young entrepreneur farmers, and they are looking for ways to get into the industry without too much hassle, and irrigation is one of the most important part of the farming process. As a result, the company decided to gain traction by breaking into the emerging smart agriculture industry in China, which framed our <i>initial problem statement</i> as
-                </p>
+                    According to the research of AI Garden, thanks to the government’s promotion of new-age sustainable agriculture there is a growing interest in the agriculture industry among the youngsters of China and they are looking for ways to get into the industry. Based on the research, the company decided to gain traction by breaking into the emerging smart agriculture industry of China with a focus on irrigation since irrigation is one of the most important part of the farming process. This framed our <i>initial problem statement</i> as</p>
                 <p className='statement'>
                     How might we provide an irrigation solution that allows younger farmers to irrigate with ease and enjoyment?
                 </p>
@@ -78,7 +78,10 @@ function AI() {
                             The irrigation process is very strenuous and time-consuming for larger farm owners, with dozens of valves to switch open and close.
                         </li>
                         <li>
-                            The younger farmers are interested in a more hands-free approach using their smart phones as they often take on other side gigs.
+                            Since there is no way to accurately monitor the water usage and pump status, it is very common for large amount of water to go wasted.
+                        </li>
+                        <li>
+                            The younger farmers are interested in a more hands-free approach using their smartphones as they often have other commitments.
                         </li>
                     </ul>
 
@@ -92,30 +95,78 @@ function AI() {
             </Text>
             <Photo source={persona2} />
 
-            <Text head={'Design'}>
+            <Text head={'Refined Problem Statement'}>
                 <p>
                     With more understanding of our users and their key goals, we refined our <i>problem statement</i> to better guide the design:
                 </p>
                 <p className='statement'>
-                    How might we provide an irrigation solution that helps the younger Chinese farmers to irrigate with less effort, time, and more accuracy? 
+                    How might we provide an irrigation solution that helps the new-age Chinese farmers to irrigate with less effort, time, and more accuracy?
+                </p>
+            </Text>
+
+            <Text head={'Solution'}>
+                <p>
+                    Guided by the new problem statement, my team and I came up with a solution that can be implemented easily on the infrastructure of most Chinese farms. The system, driven by IOT (internet of things) technology, consists of smart solenoid valves, smart water meters, and a central system unit that communicates with our mobile app.
+                </p>
+                <p>
+                    To implement the system, the trationdal valves would be swapped with smart solenoid valves that can accurately control and measure the flow of water; the mechanical water meter would be replaced with smart water meter than can accurately measure the total water usage; finally, the central system unit contained in a box would be installed near the farm. This way, the users can control which crop to irrigate, the amount of water to irrigate, as well as oversee the progress and status of the irrigation process.
                 </p>
             </Text>
 
             <Text subhead={'Wireframes'}>
                 <p>
-                    How might we provide an irrigation solution that help the younger Chines farmers to irrigate with less effort, time, and more accuracy?
+
                 </p>
             </Text>
 
             <Photo source={ai_wireframe} />
 
-            <Text subhead={'Visual Design'}>
-                <p></p>
+            <Text head={'Design'}>
+                <p>
+                    <ul>
+                        <li>
+                            A straightforward card design was used in which one card corresponds to one valve.
+                        </li>
+                        <li>
+                            Users can use the green radio buttons to select the desired valves.
+                        </li>
+                        <li>
+                            For better affordance, I emulated the physical water meter in the irrigation amount adjustment design.
+                        </li>
+                        <li>
+                            The progress of the irrigation is reflected in multiple ways including the progress bar, time elapsed, amount of water to irrigate for each valve, and the overall progress. percentage on the bottom left corner.
+                        </li>
+                        <li>
+                            To deal with the possibility of malfunction, the status of valves is displayed on the cards. When the system detects a malfunctioning valve an overlay would inform the user the malfunction, customer service number, and the fact that the system has skipped the malfunctioning valve to proceed the irrigation.
+                        </li>
+                        <li>
+                            When the irrigation is complete an overlay would inform the user the time taken, amount of water used, and amount of water preserved in the irrigation.
+                        </li>
+                    </ul>
+
+                </p>
             </Text>
 
-            <Photo source={style1} /> 
-            
+            <Text subhead={'Malfunctioning Valve Handling'}>
+                <p>
+
+                </p>
+            </Text>
+
+            <Photo source={malfunction} />
+
+            <Text subhead={'Overall Irrigation Process'}>
+                <p>
+
+                </p>
+            </Text>
+
             <img className='big-photo' src={prototype1} alt="prototype1" />
+
+            <Text subhead={'Visual Design'}>
+            </Text>
+
+            <Photo source={style1} />
 
             <Text head={'Our Team'}>
                 <p></p>

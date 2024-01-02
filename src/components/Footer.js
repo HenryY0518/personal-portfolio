d1d069e40f12465889bcdf534f2151009d15696f
Tian Yang <tianyang@Tians-MacBook-Pro.local>
2024-01-02 11:11:25 -0800

    updated resume and 2024 year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return(
         <div className='footer'>
-            <p className='footer-left'> 楊添(Henry Yang) 2021</p>
+            <p className='footer-left'> 楊添(Henry Yang) 2024</p>
             <p className='footer-right'>Made with love and tea</p>
         </div>
     )
